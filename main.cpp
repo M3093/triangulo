70a9323706568e6392851acad781a75191c1c390
Maria Badilla <>
2021-04-20 13:40:12 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+float base, altura, resultado; 
+int main() {
+ cout<<"Ingrese la base "<<endl;
+    cin>>base;
+    cout<<"Ingrese la altura"<<endl;
+    cin>>altura;
+    resultado=(base*altura)/2;
+    cout<<"El area del triangulo es :"<<resultado<<endl;
+    return 0;
+} 
